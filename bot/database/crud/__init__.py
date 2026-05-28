b5ed6bbad9a18c2f9117db2cf72a89d1facfa785
Xosilbek <koklok444@gmail.com>
2026-05-28 11:42:29 -0700

    update

--- bot/database/crud/__init__.py
+++ bot/database/crud/__init__.py
@@ -19,8 +19,10 @@ from bot.database.crud.movie import (
 from bot.database.crud.channel import (
     get_active_channels,
     get_channel_by_id,
+    get_channel_by_db_id,
     add_channel,
-    remove_channel,
+    delete_channel,
+    toggle_channel,
     get_all_channels,
 )
 from bot.database.crud.admin import (
@@ -41,8 +43,8 @@ __all__ = [
     "get_movie_by_code", "search_movies", "upsert_movie", "delete_movie",
     "record_view", "get_movies_paginated", "get_top_movies",
     # Channel
-    "get_active_channels", "get_channel_by_id", "add_channel",
-    "remove_channel", "get_all_channels",
+    "get_active_channels", "get_channel_by_id", "get_channel_by_db_id",
+    "add_channel", "delete_channel", "toggle_channel", "get_all_channels",
     # Admin
     "get_admin_by_telegram_id", "get_all_admins", "add_admin",
     "remove_admin", "is_admin", "update_admin_role",
